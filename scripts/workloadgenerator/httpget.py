fe757601494df06e1280238f46af9c31c10d827c
Thomas Kao <thomaskao@wirelessprvnat-172-17-133-10.near.illinois.edu>
2016-05-22 15:34:07 -0500

    httpget scripts

--- scripts/workloadgenerator/httpget.py
+++ scripts/workloadgenerator/httpget.py
@@ -1,25 +1,46 @@
 from __future__ import print_function
 import urllib2
+import string
+import urllib
 import sched, time
 import json
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 
-endtime = datetime.now() + timedelta(minutes=10)
+def segmentcount(log):
+	intervalarray = json.loads(urllib2.urlopen("http://node-1.druidthomas.dcsq.emulab.net:8080/druid/coordinator/v1/datasources/metrics/intervals").read())
+	totalsegments = 0
+	for interval in intervalarray:
+
+		interval2 = string.replace(interval, "/", "%2F")
+		url = "http://node-1.druidthomas.dcsq.emulab.net:8080/druid/coordinator/v1/datasources/metrics/intervals/" + interval2 + "/serverview"
+		try:
+			serverlist = json.loads(urllib2.urlopen(url).read())[0]["servers"]
+			totalsegments += len(serverlist)
+			printstatement = "Segment Interval: " + interval + ", Replicas: " + `len(serverlist)`
+			print(printstatement, file = log)
+		except IndexError:
+			continue
+	printstring = "Time: " + datetime.now().strftime('%H:%M:%S') + ", Total Segments: " + `totalsegments`
+	print(printstring, file = log)
+
+def memoryutilization(log):
+	historical1 = json.loads(urllib2.urlopen("http://node-2.druidthomas.dcsq.emulab.net:8081/status").read())
+	historical2 = json.loads(urllib2.urlopen("http://node-3.druidthomas.dcsq.emulab.net:8081/status").read())
+	historical3 = json.loads(urllib2.urlopen("http://node-4.druidthomas.dcsq.emulab.net:8081/status").read())
+
+	usedmemory = float(historical1['memory']['usedMemory']) + float(historical2['memory']['usedMemory']) + float(historical3['memory']['usedMemory'])
+	totalmemory = float(historical1['memory']['totalMemory']) + float(historical2['memory']['totalMemory']) + float(historical3['memory']['totalMemory'])
+	memoryutilization = float(usedmemory)/float(totalmemory)
+
+	printstring = "Time: " + datetime.now().strftime('%H:%M:%S')  + ", Used Memory: " + `usedmemory`
+	print(printstring, file = log)
+
+
+log1 = open("memoryusage.log", "a")
+endtime = datetime.now() + timedelta(minutes=15)
 while True:
 		if datetime.now() >= endtime:
 			break
-		historical1 = json.loads(urllib2.urlopen("http://node-2.druidthomas.dcsq.emulab.net:8081/status").read())
-		historical2 = json.loads(urllib2.urlopen("http://node-3.druidthomas.dcsq.emulab.net:8081/status").read())
-		historical3 = json.loads(urllib2.urlopen("http://node-3.druidthomas.dcsq.emulab.net:8081/status").read())
-	
-		utilization1 = float(historical1['memory']['usedMemory'])/float(historical1['memory']['totalMemory'])
-		utilization2 = float(historical2['memory']['usedMemory'])/float(historical2['memory']['totalMemory'])
-		utilization3 = float(historical3['memory']['usedMemory'])/float(historical3['memory']['totalMemory'])
 
-		log1 = open("historical1usage.txt", "a")
-		log2 = open("historical2usage.txt", "a")
-		log3 = open("historical3usage.txt", "a")
-
-		print(utilization1, file = log1)
-		print(utilization2, file = log2)
-		print(utilization3, file = log3)
+		memoryutilization(log1)
+		segmentcount(log1)
