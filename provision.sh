d288d903e1155abb9165f4aef33303731e97b7ea
Xiaoyao Qian <jivepaulrock@gmail.com>
2016-09-09 02:13:04 -0500

    log file names

--- provision.sh
+++ provision.sh
@@ -1,40 +1,10 @@
 #!/bin/bash -e
 
-# Create kv store for service discovery
-# docker-machine -D create --driver virtualbox mh-keystore
-# eval "$(docker-machine env mh-keystore)"
-# docker run -d \
-#   -p "8500:8500" \
-#   -h "consul" \
-#   progrium/consul -server -bootstrap
-
-# Create swarm master
-# docker-machine -D create --driver virtualbox \
-#   --swarm \
-#   --swarm-master \
-#   --swarm-discovery="consul://$(docker-machine ip mh-keystore):8500" \
-#   --engine-opt="cluster-store=consul://$(docker-machine ip mh-keystore):8500" \
-#   --engine-opt="cluster-advertise=eth1:2376" \
-#   druid-swarm-master
-
-# Create swarm workers
-# for i in `seq 1 2`;
-# do
-#   echo $i
-#   docker-machine -D create --driver virtualbox \
-#     --swarm \
-#     --swarm-discovery="consul://$(docker-machine ip mh-keystore):8500" \
-#     --engine-opt="cluster-store=consul://$(docker-machine ip mh-keystore):8500" \
-#     --engine-opt="cluster-advertise=eth1:2376" \
-#     node-$i
-# done
-
-# Create overlay network
-# eval $(docker-machine env --swarm druid-swarm-master)
-# docker network create --driver overlay --subnet=10.0.9.0/24 my-net
-
-
-
+# Change shell to bash
+for i in `seq 1 12`;
+do
+  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no xiaoyao@node-$i.druidxiaoyao.dcsq.emulab.net "sudo chsh -s /bin/bash xiaoyao"
+done
 
 # Create kv store for service discovery
 docker-machine -D create --driver generic \
