015577397cef8de6935b56da035a3d9da2b4b836
Le Xu <lexu@node-1.druid2.dcsq.emulab.net>
2016-09-24 13:34:26 -0600

    m

--- scripts/workloadgenerator/Query/QueryGenerator.py
+++ scripts/workloadgenerator/Query/QueryGenerator.py
@@ -71,8 +71,8 @@ class QueryGenerator(object):
 	     			fullist.append([period_start, period_end])
 	     			#print "add entry: ", period_start
 
-    		for k in xrange(days):
-    			dateaccessed.append(period_start+relativedelta(days=k))
+    			for k in xrange(days):
+    				dateaccessed.append(period_start+relativedelta(days=k))
 
 		dateaccessed.sort()
 		originallength = (dateaccessed[len(dateaccessed)-1]-dateaccessed[0]).total_seconds()
@@ -98,57 +98,20 @@ class QueryGenerator(object):
 			newstart = truestart
 			startstring = newstart.strftime('%Y-%m-%dT%H:%M:%S')
 			period_interval_inSec = (originallist[i][1]-originallist[i][0]).total_seconds()
-		print "original period in seconds", period_interval_inSec
-		factor = (originallength)/(float(truelength))
-		period_interval_inSec = (period_interval_inSec)/(float(factor))
-		if period_interval_inSec < 1.0:
-			period_interval_inSec = 1
-		period_interval_inSec = int(period_interval_inSec)
+			print "original period in seconds", period_interval_inSec
+			factor = (originallength)/(float(truelength))
+			period_interval_inSec = (period_interval_inSec)/(float(factor))
+			if period_interval_inSec < 1.0:
+				period_interval_inSec = 1
+			period_interval_inSec = int(period_interval_inSec)
 
-		print "final period in seconds", period_interval_inSec
+			print "final period in seconds", period_interval_inSec
 
-		'''if(period_interval_inSec < 31536000):
-			if(period_interval_inSec < 2592000):
-				if(period_interval_inSec < 604800):
-					if(period_interval_inSec < 86400):
-						if(period_interval_inSec < 3600):
-							if(period_interval_inSec < 60):
-							 	duration = period_interval_inSec
-						 	 	q.setInterval(startstring + "/pt" + str(duration) + "s")
-					 	else:
-					 	 	duration = period_interval_inSec/60
-						 	 	q.setInterval(startstring + "/pt" + str(duration) + "m")
-					else:
-							duration = period_interval_inSec/3600
-						 	q.setInterval(startstring + "/pt" + str(duration) + "h")
-					else:
-						duration = period_interval_inSec/86400
-						q.setInterval(startstring + "/p" + str(duration) + "d")
-				else:
-					duration = period_interval_inSec/604800
-					q.setInterval(startstring + "/p" + str(duration) + "w")
-
-			else:
-				duration = period_interval_inSec/2592000
-				q.setInterval(startstring + "/p" + str(duration) + "m")
-
-		else:
-			duration = period_interval_inSec/31536000
-			q.setInterval(startstring + "/p" + str(duration) + "y")
-		'''
 			#for count1 in range(0, period_count):
 			#	querylist.append(q)
 				#print "interval: " + q.interval
-		q.setInterval(startstring + "/" + Utils.iso8601(dt.timedelta(seconds=period_interval_inSec)))
-		querylist.append(q)
-		print "interval: " + q.interval
-		print "index: " , q.index
-		print "starttime: " , q.startTime
-		#querycount=len(fulllist)
-		#for count2 in xrange(numQueries):
-		#	querylist.append(fulllist[random.randint(0,querycount-1)])
-		#	print "interval: " + querylist[len(querylist)-1].interval
-		#	print "index: " , querylist[len(querylist)-1].index
-		#	print "starttime: ", querylist[len(querylist)-1].startTime
-
+			q.setInterval(startstring + "/" + Utils.iso8601(dt.timedelta(seconds=period_interval_inSec)))
+			querylist.append(q)
+			print "interval: " + q.interval
+			print "index: " , q.index
 		return querylist
