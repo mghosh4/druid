3a972555251db97bd5b2e3f156aacc6cd383c56e
Ashwini Raina <raina4@illinois.edu>
2017-07-14 14:51:38 -0600

    fixed scripts

--- scripts/workloadgenerator/ingestAndRunWorkloadGen_raina4.sh
+++ scripts/workloadgenerator/ingestAndRunWorkloadGen_raina4.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-COMMAND="cd /proj/DCSQ/raina4/druid/scripts/ingestion/;"
+COMMAND="cd /proj/DCSQ/raina4/potion_exp/druid/scripts/ingestion/;"
 COMMAND=$COMMAND" screen -d -m python ingestion.py getafix.conf;"
 echo $COMMAND
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no node-1 "$COMMAND"
@@ -11,7 +11,7 @@ sleep 60
 
 sleep 60
 
-COMMAND="cd /proj/DCSQ/raina4/druid/scripts/workloadgenerator/;"
+COMMAND="cd /proj/DCSQ/raina4/potion_exp/druid/scripts/workloadgenerator/;"
 COMMAND=$COMMAND" screen -d -m bash checkAndLog_raina4.sh $@;"
 echo $COMMAND
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no node-1 "$COMMAND"
