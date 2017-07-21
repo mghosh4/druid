1b6f62dd7537263a2786dff53bcc4a500085fb63
raina4 <raina4@illinois.edu>
2017-07-21 18:17:04 -0500

    fixed plotting scripts

--- scripts/plotscripts/hnQuerySegmentTime.sh
+++ scripts/plotscripts/hnQuerySegmentTime.sh
@@ -1,9 +1,4 @@
 #!/bin/bash -e
 
 # Scripts prints the query/segment/time processed at each historical nodes.
-# It assumes that 15 historical nodes are configured
-
-for ((i=0; i<15; i++))
-do
-grep query/segment/time historical-$i.log | wc -l
-done
+grep query/segment/time historical-*.log | wc -l
