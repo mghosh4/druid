c089d5ec3dae4e8d40db5fe8cbb51e9f567b8d99
Xiaoyao Qian <jivepaulrock@gmail.com>
2017-09-01 16:18:32 -0500

    init

--- scripts/ec2_scripts/startcmd.sh
+++ scripts/ec2_scripts/startcmd.sh
@@ -2,17 +2,17 @@
 
 echo "Setting up the harddisk"
 mkfs -t ext4 /dev/xvdf
-mkdir /druid
-mount /dev/xvdf /druid/
-sudo chown ubuntu:ubuntu /druid
+mkdir /proj
+mount /dev/xvdf /proj/
+chown ubuntu:ubuntu /proj
 
 echo "Setting up passwordless ssh"
 echo 'alias ssh="ssh -i ~/druid.pem"' >> /home/ubuntu/.bash_aliases
 
 echo "Installing packages"
 apt-get update
-apt-get install -y openjdk-7-jre
-apt-get install -y openjdk-7-jdk
+apt-get install -y openjdk-8-jre
+apt-get install -y openjdk-8-jdk
 apt-get install -y curl
 apt-get install -y screen
 apt-get install -y wget
@@ -24,6 +24,13 @@ apt-get install -y maven
 apt-get install -y python-pip
 apt-get install -y python-pandas
 apt-get install -y libcurl4-openssl-dev
+apt-get install -y nfs-common
 pip install pydruid pytz tornado pycurl
 
 bash -c "export DEBIAN_FRONTEND=noninteractive; apt-get -q -y install mysql-server"
+
+# NFS
+mkdir /efsdata
+mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 {efsIP}:/ /efsdata
+chown ubuntu:ubuntu /efsdata
+chmod go+rw /efsdata
