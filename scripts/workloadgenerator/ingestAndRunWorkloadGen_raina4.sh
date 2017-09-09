92ed8c2165a754d27001a324f0809600eb7f128b
Ashwini Raina <raina4@illinois.edu>
2017-09-09 13:35:39 -0600

    script time updates to *_raina4.sh files

--- scripts/workloadgenerator/ingestAndRunWorkloadGen_raina4.sh
+++ scripts/workloadgenerator/ingestAndRunWorkloadGen_raina4.sh
@@ -5,11 +5,10 @@ COMMAND=$COMMAND" screen -d -m python ingestion.py getafix.conf;"
 echo $COMMAND
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no node-1 "$COMMAND"
 
-sleep 60
+#Sleep for two minutes so that one segment is created and loaded before we start querying. Removed the sleep from workload generator
+sleep 2m
 
 ./runMultiWorkloadGen_raina4.sh $@
-
-sleep 60
 
 COMMAND="cd /proj/DCSQ/raina4/druid/scripts/workloadgenerator/;"
 COMMAND=$COMMAND" screen -d -m bash checkAndLog_raina4.sh $@;"
